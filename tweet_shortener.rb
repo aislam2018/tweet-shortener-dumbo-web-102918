930ecf0d2c5669e66fb9a17cec6fe2515c2d25a1
anik1606 <anik1606@gmail.com>
2018-10-17 22:43:57 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,16 +1,16 @@
 require "pry"
 def dictionary 
  dictionary = {
-"hello": "hi",
-"to": "2", 
-"two": "2",  
-"too": "2",  
-"for": "4",
-"four": "4", 
-"be": "b",
-"you": "u",
-"at": "@", 
-"and": "&"
+"hello"=> "hi",
+"to"=> "2", 
+"two"=> "2",  
+"too"=> "2",  
+"for"=> "4",
+"four"=> "4", 
+"be"=> "b",
+"you"=> "u",
+"at"=> "@", 
+"and"=> "&"
 }
 end
 
