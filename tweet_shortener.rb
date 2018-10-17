ea8151851a9e0d8ce6def9777b3c4a417e98f509
anik1606 <anik1606@gmail.com>
2018-10-17 21:18:33 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def dictionary 
+  {
+"hello": "hi",
+"to": "2", 
+"two": "2",  
+"too": "2",  
+"for": "4",
+"four": "4", 
+"be": "b",
+"you": "u",
+"at": "@", 
+"and": "&"
+}
+end
+
+def word_substituter(str)
+  new_words = []
+  words = str.split(" ")
+  words.each do |word|
+    dictionary.each do |key, val|
+    if key == word
+      new_words.push(val)
+    else
+      new_words.push(word)
+    end
+    end
+  end
+  new_words.join(" ")
+end
